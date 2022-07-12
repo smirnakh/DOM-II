3fd6b621cd718a6d60cd04bd0983f7e8f25f19b7
smirnakh <99379073+smirnakh@users.noreply.github.com>
2022-07-12 12:49:24 -0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './less/index.less'
 
 // Your code goes here!
+window.onload = function(event) {
+const heading = document.querySelector("h1");
+heading.textContent = "Ready To Go"
+
+window.addEventListener("copy", () => {
+    navigator.clipboard.readText()
+    .then(text => {
+        heading.textContent += text;
+    });
+})
+document.body.addEventListener("click", (event) => {
+ event.target.classList.toggle("mirror")
+})
+
+document.body.addEventListener("dblclick", (event) => {
+    event.target.innerHTML = ""
+})
+
+window.addEventListener("keydown", (event) => {
+    if (event.key == 6){
+        document.body.innerHTML = "<h1>YOU'VE DELETED THE WEBSITE ; )</h1>"
+    }
+    })
+}
+
+
